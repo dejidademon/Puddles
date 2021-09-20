7d51c7dbf29538354dde684df674d4b6d186d8d2
Adedeji Oyefeso <gradeprison@gmail.com>
2021-09-20 13:28:00 -0400

    Drops Pg Start

--- src/pages/NewDrops.vue
+++ src/pages/NewDrops.vue
@@ -1,9 +1,20 @@
 <template>
-  <q-page style="background-color: #6f7b87">
+  <q-page >
     <h1 class="title text-center">New Arrivals</h1>
 
     <div class="q-pa-md">
-      <q-carousel animated :autoplay="autoplay" v-model="slide" arrows swipeable infinite transition-next="slide-left" transition-prev="slide-right" control-color="white">
+      <q-carousel
+        class="carousel"
+        animated
+        :autoplay="autoplay"
+        v-model="slide"
+        arrows
+        swipeable
+        infinite
+        transition-next="slide-left"
+        transition-prev="slide-right"
+        control-color="white"
+      >
         <q-carousel-slide
           :name="1"
           img-src="https://cdn.quasar.dev/img/mountains.jpg"
@@ -22,16 +33,52 @@
         />
       </q-carousel>
     </div>
+    <div>
+<q-card>
+  <q-card-section>
+
+      <q-carousel
+        class="carousel"
+        animated
+        :autoplay="autoplay"
+        v-model="slide"
+        arrows
+        swipeable
+        infinite
+        transition-next="slide-left"
+        transition-prev="slide-right"
+        control-color="white"
+      >
+        <q-carousel-slide
+          :name="1"
+          img-src="https://cdn.quasar.dev/img/mountains.jpg"
+        />
+        <q-carousel-slide
+          :name="2"
+          img-src="https://cdn.quasar.dev/img/parallax1.jpg"
+        />
+        <q-carousel-slide
+          :name="3"
+          img-src="https://cdn.quasar.dev/img/parallax2.jpg"
+        />
+        <q-carousel-slide
+          :name="4"
+          img-src="https://cdn.quasar.dev/img/quasar.jpg"
+        />
+      </q-carousel>
+  </q-card-section>
+</q-card>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 export default {
   data() {
     return {
       slide: 1,
-      autoplay: true
+      autoplay: true,
     };
   },
 };
@@ -41,6 +88,8 @@ export default {
 .title {
   font-family: "puddles_font";
   color: white;
+  line-height: unset;
+  white-space: nowrap;
 }
 
 /* big */
@@ -48,16 +97,33 @@ export default {
   .title {
     margin: -5px;
     font-size: 50px;
-    line-height: normal;
+  }
+  .carousel {
+    height: 350px;
   }
 }
 //smaller screen
 @media screen and (max-width: 970px) {
+    .carousel {
+    height: 300px;
+  }
+  .title {
+    margin: -5px;
+    font-size: 30px;
+  }
 }
 // tablet
 @media screen and (max-width: 640px) {
+    .title {
+    margin: -5px;
+    font-size: 30px;
+  }
 }
 //mobile
 @media screen and (max-width: 440px) {
+    .title {
+    margin: 5px;
+    font-size: 22px;
+  }
 }
 </style>
