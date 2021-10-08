6c0d1b8b8cdd67e78115d2cb51d677aadffbaa57
dejidademon <gradeprison@gmail.com>
2021-10-07 20:55:09 -0400

    backend test

--- backend/index.js
+++ backend/index.js
@@ -42,11 +42,7 @@ app.get('/slides', (request, response) => {
               "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1621283126-t-shirts-buckmason-black-slub-1621282639.jpg",
             itemImg2:
               "https://imgprd19.hobbylobby.com/0/2d/95/02d95aaa2491fc88832ff0fca3388b37d29309de/350Wx350H-709618-0320.jpg",
-            itemImg3:
-              "https://cdn.shopify.com/s/files/1/1722/0531/products/modern-love-shirt-roller_1024x1024.jpg?v=1571439091",
-            itemImg4:
-              "https://www.ramblersway.com/sites/default/files/product_photos/708-Western-Chambray-Shirt.jpg",
-            itemPreview: false,
+
             datePosted: "10/5/2021",
           }
     ]
