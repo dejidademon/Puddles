67bd6d71c83b454e45824544e31ab042e32c4261
dejidademon <gradeprison@gmail.com>
2021-10-17 19:52:41 -0400

    WORKING BACKEND

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+
 /*
 dependencies
 */
@@ -8,46 +9,34 @@ config-express
 */
 const app = express()
 
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const serviceAccount = require('./serviceAccountKey.json');
 
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
 /*
 end of slides
 */
 
 app.get('/slides', (request, response) => {
-    let slides = [
-        {
-            itemName: "Puddles Longsleeve",
-            itemPrice: "$29.99",
-            itemSlide: 1,
-            itemDesc:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed maximus diam. Ut volutpat accumsan enim sed mollis. Aliquam condimentum non leo eu tempus. Nulla at nisl et eros lacinia aliquam vitae sit amet ligula. In fringilla dignissim placerat. Aenean ullamcorper tellus vitae quam malesuada, rhoncus faucibus purus fermentum.",
-            itemImg1:
-              "https://imgprd19.hobbylobby.com/0/2d/95/02d95aaa2491fc88832ff0fca3388b37d29309de/350Wx350H-709618-0320.jpg",
-            itemImg2:
-              "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1621283126-t-shirts-buckmason-black-slub-1621282639.jpg",
-            itemImg3:
-              "https://cdn.shopify.com/s/files/1/1722/0531/products/modern-love-shirt-roller_1024x1024.jpg?v=1571439091",
-            itemImg4:
-              "https://www.ramblersway.com/sites/default/files/product_photos/708-Western-Chambray-Shirt.jpg",
-            itemPreview: false,
-            datePosted: "10/5/2021",
-          },
-          {
-            itemName: "Puddles T-Shirt",
-            itemPrice: "$19.99",
-            itemSlide: 1,
-            itemDesc:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed maximus diam. Ut volutpat accumsan enim sed mollis. Aliquam condimentum non leo eu tempus. Nulla at nisl et eros lacinia aliquam vitae sit amet ligula. In fringilla dignissim placerat. Aenean ullamcorper tellus vitae quam malesuada, rhoncus faucibus purus fermentum.",
-            itemImg1:
-              "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1621283126-t-shirts-buckmason-black-slub-1621282639.jpg",
-            itemImg2:
-              "https://imgprd19.hobbylobby.com/0/2d/95/02d95aaa2491fc88832ff0fca3388b37d29309de/350Wx350H-709618-0320.jpg",
 
-            datePosted: "10/5/2021",
-          }
-    ]
+
+
+    let slides = []
     
-  response.send(slides)
+        db.collection('Slides').get().then(r => {
+          r.docs.map((doc => {
+            slides.push(doc.data())
+          }));
+
+          response.send(slides)
+        })
+    
+
 
 })
 
