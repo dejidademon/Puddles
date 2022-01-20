cdf61bd76341fcfe1253370174e76118946cdda0
Adedeji Oyefeso <gradeprison@gmail.com>
2022-01-19 19:56:29 -0500

    work time admin page

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -2,7 +2,7 @@
     <q-page class="q-ma-lg">
  <h2 class="title q-pb-sm text-center">Admin Account</h2>
 
-    <h4 class="subtitle puddlesText text-center q-ma-sm">Order Statistics</h4>
+    <h4 class="subtitle puddlesText text-center q-ma-sm">Item Statistics</h4>
         <div class="statContainer overflow-auto hide-scrollbar">
         <q-list class="column">
        <stats />
@@ -43,6 +43,54 @@
                 loadingOrders: null,
             }
         },
+        
+        methods: {
+    getOrders() {
+      this.loadingOrders = true;
+
+        axios
+          .get(`${process.env.API}/purchases`)
+          .then((r) => {
+              this.favItems = JSON.parse(JSON.stringify(r.data));
+              this.items = JSON.parse(JSON.stringify(r.data));
+              // console.log(orginData)
+          })
+          .catch((err) => {
+            this.$q.dialog({
+              style: "background-color:red;",
+              dark: true,
+              color: "white",
+              title: "Error",
+              message: "Could not get any items",
+              persistent: true,
+            });
+          });
+          this.loadingOrders = false;
+    },
+    
+        getStats() {
+      this.loadingStats = true;
+
+        axios
+          .get(`${process.env.API}/slides`)
+          .then((r) => {
+              this.items = JSON.parse(JSON.stringify(r.data));
+              // console.log(orginData)
+          })
+          .catch((err) => {
+            this.$q.dialog({
+              style: "background-color:red;",
+              dark: true,
+              color: "white",
+              title: "Error",
+              message: "Could not get any items",
+              persistent: true,
+            });
+          });
+          this.loadingStats = false;
+    },
+        },
+
         components: {
             'stats': require("src/components/Account/Statistics.vue").default,
             'orders': require("src/components/Account/Orders.vue").default,
@@ -77,6 +125,7 @@
   color: white;
   font-size: 25px;
 }
+
 /* big */
 @media screen and (min-width: 970px) {
     //btns
@@ -105,6 +154,20 @@
 }
 //smaller screen
 @media screen and (max-width: 970px) {
+      .statBtns {
+  width: 70px;
+  height: 70px ;
+ 
+    }
+.addBtn {
+  font-size: 50px;     
+    overflow: hidden;
+    margin: 0;
+}
+.deleteBtn {
+     font-size: 50px;     
+    display: block;
+}
   .subtitle {
   font-size: 20px;
 }
